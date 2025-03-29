718007b510599c53c0559cec4494d1b679c2afff
xiomi94 <xiomarajimvel94@gmail.com>
2025-03-29 14:31:11 +0000

    Create access to admin missions

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,6 +1,13 @@
 import './Footer.css'
+import {useState} from "react";
+import {useNavigate} from "react-router";
+import CommandModal from "../command-modal/CommandModal.tsx";
 
 function Footer() {
+
+  const [commandUser, setCommandUser] = useState<string[]>([])
+  const [showCommandUser, setShowCommandUser] = useState<boolean>(false)
+  const navigate = useNavigate()
 
   const navigateNewTab = (url: string) => {
     const newTab = window.open(url, "_blank")
@@ -9,12 +16,54 @@ function Footer() {
     }
   }
 
-  return(
-    <footer className="footer-container">
-        <button className="footer-button-icons" onClick={() => {navigateNewTab("https://github.com/xiomi94/helldivers-project")}}>
+  const onClickCommand= (command: string) => {
+    const newCommandUser = [command, ...commandUser]
+    const commandForAccessAdminMission = ['up', 'left', 'right', 'down', 'up']
+
+    if (newCommandUser.length > 5) {
+      newCommandUser.splice(5)
+    }
+
+    let navigateToAdminMission = true
+
+    if (newCommandUser.length < 5) {
+      navigateToAdminMission = false
+    } else {
+      for (let i = 0; i < commandForAccessAdminMission.length; i++) {
+        if (commandForAccessAdminMission[i] !== newCommandUser[i]) {
+          navigateToAdminMission = false
+          break;
+        }
+      }
+    }
+
+    if (navigateToAdminMission) {
+      setShowCommandUser(false)
+      navigate('/admin-missions')
+    }
+
+    setCommandUser(newCommandUser)
+  }
+
+  const onClickShowCommandUser = () => {
+    setShowCommandUser(true)
+    alert("Comando para entrar al administrador de misions: ARRIBA, ABAJO, DERECHA, IZQUIERDA, ARRIBA")
+  }
+
+  return (
+    <>
+      <footer className="footer-container">
+        <button className="footer-button-icons">
+          <i className="bi bi-shield" onClick={onClickShowCommandUser}></i>
+        </button>
+        <button className="footer-button-icons" onClick={() => {
+          navigateNewTab("https://github.com/xiomi94/helldivers-project")
+        }}>
           <i className="bi bi-github"></i>
         </button>
-        <button className="footer-button-icons" onClick={() => {navigateNewTab("https://www.instagram.com/helldivers2game/?hl=es")}}>
+        <button className="footer-button-icons" onClick={() => {
+          navigateNewTab("https://www.instagram.com/helldivers2game/?hl=es")
+        }}>
           <i className="bi bi-instagram"></i>
         </button>
         <button className="footer-button-icons" onClick={() => {
@@ -22,7 +71,10 @@ function Footer() {
         }}>
           <i className="bi bi-wikipedia"></i>
         </button>
-    </footer>
+      </footer>
+      <CommandModal setShowCommandUser={setShowCommandUser} onClickCommand={(command: string) => {onClickCommand(command)}} show={showCommandUser}/>
+    </>
+
   )
 }
 
